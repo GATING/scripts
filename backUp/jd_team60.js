daf3735f7d45048294f802cfb2bfe782f1b17f2f
gating <674964222@qq.com>
2022-01-21 15:32:23 +0800

    微定制瓜分京豆已经没水了

--- backUp/jd_team60.js
+++ backUp/jd_team60.js
@@ -14,8 +14,8 @@ function openCardActivity(activityId, activityUrl, pin, num, againUserIndex) {
   return new Promise((resolve) => {
     const prefix = activityUrl.includes("cjhydz") ? "cjhydz" : "lzkjdz";
     const $ = new Env("微定制瓜分京豆");
-    const notify = $.isNode() ? require("./sendNotify") : "";
-    const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
+    const notify = $.isNode() ? require("../sendNotify") : "";
+    const jdCookieNode = $.isNode() ? require("../jdCookie.js") : "";
     let cookiesArr = [],
       cookie = "",
       message = "",
