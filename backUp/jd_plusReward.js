767d3b40f023038ab581f110be2510235f6c733a
gating <674964222@qq.com>
2022-03-18 16:13:13 +0800

    删除无效脚本

--- backUp/jd_plusReward.js
+++ backUp/jd_plusReward.js
@@ -3,9 +3,9 @@
 TG https://t.me/okyydsnb
 */
 const $ = new Env("逛plus，抽京豆");
-const notify = $.isNode() ? require("./sendNotify") : "";
+const notify = $.isNode() ? require("../sendNotify") : "";
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
+const jdCookieNode = $.isNode() ? require("../jdCookie.js") : "";
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [],
   cookie = "";
