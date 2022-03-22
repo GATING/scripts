79cb0bc76ddaa48cb02167e7b7d7669b8c43f0e2
gating <674964222@qq.com>
2022-03-22 14:26:19 +0800

    删除无效任务

--- backUp/jd_desire.js
+++ backUp/jd_desire.js
@@ -10,9 +10,9 @@ cron:30 6 * * *
  */
 
 const $ = new Env("京东集魔方");
-const notify = $.isNode() ? require("./sendNotify") : "";
+const notify = $.isNode() ? require("../sendNotify") : "";
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
+const jdCookieNode = $.isNode() ? require("../jdCookie.js") : "";
 let jdNotify = true; //是否关闭通知，false打开通知推送，true关闭通知推送
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [],
@@ -413,7 +413,7 @@ function taskPostUrl(functionId, body = {}) {
       "User-Agent": $.isNode()
         ? process.env.JD_USER_AGENT
           ? process.env.JD_USER_AGENT
-          : require("./USER_AGENTS").USER_AGENT
+          : require("../USER_AGENTS").USER_AGENT
         : $.getdata("JDUA")
         ? $.getdata("JDUA")
         : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
@@ -500,7 +500,7 @@ function TotalBean() {
         "User-Agent": $.isNode()
           ? process.env.JD_USER_AGENT
             ? process.env.JD_USER_AGENT
-            : require("./USER_AGENTS").USER_AGENT
+            : require("../USER_AGENTS").USER_AGENT
           : $.getdata("JDUA")
           ? $.getdata("JDUA")
           : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
