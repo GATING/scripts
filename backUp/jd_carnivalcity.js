acd3b64b31d67665c452906fcdeb97c6c5c873b8
gating <674964222@qq.com>
2022-04-21 00:43:00 +0800

    京东手机狂欢城活动过期

--- backUp/jd_carnivalcity.js
+++ backUp/jd_carnivalcity.js
@@ -15,8 +15,8 @@ cron "0 0-18/6 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/syn
 */
 
 const $ = new Env("京东手机狂欢城");
-const notify = $.isNode() ? require("./sendNotify") : "";
-const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
+const notify = $.isNode() ? require("../sendNotify") : "";
+const jdCookieNode = $.isNode() ? require("../jdCookie.js") : "";
 let cookiesArr = [],
   cookie = "",
   message = "",
@@ -755,7 +755,7 @@ function taskUrl(body = {}) {
       "User-Agent": $.isNode()
         ? process.env.JD_USER_AGENT
           ? process.env.JD_USER_AGENT
-          : require("./USER_AGENTS").USER_AGENT
+          : require("../USER_AGENTS").USER_AGENT
         : $.getdata("JDUA")
         ? $.getdata("JDUA")
         : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
@@ -872,7 +872,7 @@ function updateShareCodesCDN() {
           "User-Agent": $.isNode()
             ? process.env.JD_USER_AGENT
               ? process.env.JD_USER_AGENT
-              : require("./USER_AGENTS").USER_AGENT
+              : require("../USER_AGENTS").USER_AGENT
             : $.getdata("JDUA")
             ? $.getdata("JDUA")
             : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
@@ -910,7 +910,7 @@ function taskPostUrl(a, t = {}) {
       "User-Agent": $.isNode()
         ? process.env.JD_USER_AGENT
           ? process.env.JD_USER_AGENT
-          : require("./USER_AGENTS").USER_AGENT
+          : require("../USER_AGENTS").USER_AGENT
         : $.getdata("JDUA")
         ? $.getdata("JDUA")
         : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
@@ -1425,7 +1425,7 @@ function getFarmShareCode(cookie) {
           "User-Agent": $.isNode()
             ? process.env.JD_USER_AGENT
               ? process.env.JD_USER_AGENT
-              : require("./USER_AGENTS").USER_AGENT
+              : require("../USER_AGENTS").USER_AGENT
             : $.getdata("JDUA")
             ? $.getdata("JDUA")
             : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
@@ -1458,7 +1458,7 @@ function getBeanShareCode(cookie) {
           "User-Agent": $.isNode()
             ? process.env.JD_USER_AGENT
               ? process.env.JD_USER_AGENT
-              : require("./USER_AGENTS").USER_AGENT
+              : require("../USER_AGENTS").USER_AGENT
             : $.getdata("JDUA")
             ? $.getdata("JDUA")
             : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
