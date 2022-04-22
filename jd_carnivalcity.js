b3a4d6bbc86bc6f62c266412ff7c8ea6ed7d1be9
gating <674964222@qq.com>
2022-04-22 18:05:44 +0800

    update

--- jd_carnivalcity.js
+++ jd_carnivalcity.js
@@ -4,14 +4,14 @@
 ===================quantumultx================
 [task_local]
 #京东手机狂欢城
-0 0-18/6 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_carnivalcity.js, tag=京东手机狂欢城, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+0 0-18/6 * * * https://raw.githubusercontent.com/222222/sync/jd_scripts/jd_carnivalcity.js, tag=京东手机狂欢城, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 =====================Loon================
 [Script]
-cron "0 0-18/6 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_carnivalcity.js, tag=京东手机狂欢城
+cron "0 0-18/6 * * *" script-path=https://raw.githubusercontent.com/222222/sync/jd_scripts/jd_carnivalcity.js, tag=京东手机狂欢城
 ====================Surge================
-京东手机狂欢城 = type=cron,cronexp=0 0-18/6 * * *,wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_carnivalcity.js
+京东手机狂欢城 = type=cron,cronexp=0 0-18/6 * * *,wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/222222/sync/jd_scripts/jd_carnivalcity.js
 ============小火箭=========
-京东手机狂欢城 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_carnivalcity.js, cronexpr="0 0-18/6 * * *", timeout=3600, enable=true
+京东手机狂欢城 = type=cron,script-path=https://raw.githubusercontent.com/222222/sync/jd_scripts/jd_carnivalcity.js, cronexpr="0 0-18/6 * * *", timeout=3600, enable=true
 */
 
 const $ = new Env("京东手机狂欢城");
@@ -67,7 +67,6 @@ let nowTime =
       );
     return;
   }
-  await updateShareCodesCDN();
 
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
@@ -81,9 +80,6 @@ let nowTime =
       getUA();
       await supportList(); //助力情况
       await getHelp(); //获取邀请码
-      if ($.updatePkActivityIdRes && $.updatePkActivityIdRes.length) {
-        $.temp = [...new Set([...$.temp, ...$.updatePkActivityIdRes])];
-      }
     }
   }
 
@@ -860,41 +856,6 @@ function getListRank() {
         resolve(data);
       }
     });
-  });
-}
-
-function updateShareCodesCDN() {
-  return new Promise((resolve) => {
-    $.get(
-      {
-        url: "https://api.jdsharecode.xyz/api/carnivalcity/30",
-        headers: {
-          "User-Agent": $.isNode()
-            ? process.env.JD_USER_AGENT
-              ? process.env.JD_USER_AGENT
-              : require("./USER_AGENTS").USER_AGENT
-            : $.getdata("JDUA")
-            ? $.getdata("JDUA")
-            : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
-        },
-        timeout: 10000,
-      },
-      async (err, resp, data) => {
-        try {
-          if (err) {
-            console.log(`${JSON.stringify(err)}`);
-            console.log(`${$.name} API请求失败，请检查网路重试`);
-          } else {
-            data = JSON.parse(data);
-            $.updatePkActivityIdRes = data.data;
-          }
-        } catch (e) {
-          $.logErr(e, resp);
-        } finally {
-          resolve();
-        }
-      }
-    );
   });
 }
 
